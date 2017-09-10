da91223751e0666d4244cdebcac7fe3e5390a4bb
Nadav Ben-Amram <nadavbenamram@gmail.com>
2017-09-10 20:43:10 +0300

    Player script added

---
+++ RollingStone/RollingStone/Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class Player : MonoBehaviour 
+{
+	private const float TIME_FOR_POINT = 1f;
+
+	private int m_Score;
+	private Stopwatch m_StopWatch;
+	private float m_NextTimeForAddingPoint;
+
+	// Use this for initialization
+	void Start() 
+	{
+		DontDestroyOnLoad (gameObject);
+		m_Score = 0;
+		m_StopWatch = new Stopwatch();
+		m_StopWatch.Start();
+		m_NextTimeForAddingPoint = TIME_FOR_POINT;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		addPointToPlayerOrDecreaseTimer ();
+	}
+
+	private void addPointToPlayerOrDecreaseTimer()
+	{
+		m_NextTimeForAddingPoint -= Time.deltaTime;
+		if (m_NextTimeForAddingPoint <= 0) 
+		{
+			m_NextTimeForAddingPoint = TIME_FOR_POINT + m_NextTimeForAddingPoint;
+		}
+	}
+}
